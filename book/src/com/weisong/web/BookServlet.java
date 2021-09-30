98496f3aec1e9d4016c51f07e2e29871a0a6d17d
liweisong-java <liweisong0628@gmail.com>
2021-09-30 17:25:07 +0800

    图书列表功能的实现

---
+++ book/src/com/weisong/web/BookServlet.java
@@ -0,0 +1,41 @@
+package com.weisong.web;
+
+import com.weisong.pojo.Book;
+import com.weisong.service.BookService;
+import com.weisong.service.impl.BookServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author 李伟松
+ * @create 2021-09-30-16:33
+ */
+public class BookServlet extends BaseServlet{
+
+    private BookService bookService = new BookServiceImpl();
+
+    protected void add(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    protected void delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    protected void update(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    protected void list(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //1.通过BookService查询全部图书
+        List<Book> books = bookService.queryBooks();
+        //2.把全部图书保存到Request域中
+        req.setAttribute("books", books);
+        //3.请求转发到/pages/manager/book_manager.jsp页面
+        req.getRequestDispatcher("/pages/manager/book_manager.jsp").forward(req, resp);
+    }
+}
